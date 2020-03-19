81e58e6438162553f3d266c6665f29b3e3e9e7bf
aluno20472 <48324625+aluno20472@users.noreply.github.com>
2020-03-19 22:39:41 +0000

    Changes on some routes for no authentication

--- backend_laravel/app/Http/Controllers/RouteController.php
+++ backend_laravel/app/Http/Controllers/RouteController.php
@@ -8,7 +8,8 @@ class RouteController extends Controller
 {
 
     public function index(Request $request){
-
+        $routes = Route::paginate(15);
+        return response()->json($routes, 200);
     }
 
     public function show($id){
@@ -19,11 +20,21 @@ class RouteController extends Controller
 
     }
 
-    public function update($id,Request $request){
+    public function update($id, Request $request){
+        $route = User::findOrFail($id);
 
+        $this->validate($request, [
+            'name'=>'nullable|string'
+        ]);
+
+        $route->update($request->only(['name']));
+        $route->save();
+        return response()->json(['route' => $route], 200);
     }
 
     public function delete($id){
-
+        $user = User::findOrFail($user);
+        $response = $user->delete();
+        return response()->json($response, 200);
     }
 }
