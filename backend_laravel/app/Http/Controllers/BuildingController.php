2f7caea9c6a47b54c9c544d691cf6089f7a47cd0
tugamars <mars.silva.13@gmail.com>
2020-03-20 00:35:38 +0000

    Added method to store new Building (and associated stuff)

--- backend_laravel/app/Http/Controllers/BuildingController.php
+++ backend_laravel/app/Http/Controllers/BuildingController.php
@@ -9,6 +9,9 @@
 namespace App\Http\Controllers;
 
 
+use App\Building;
+use App\Image;
+use App\Vertice;
 use Illuminate\Http\Request;
 
 class BuildingController extends Controller
@@ -23,6 +26,68 @@ class BuildingController extends Controller
     }
 
     public function store(Request $request){
+
+        $rules = [
+
+            //Building
+            'buildingName'=>'required|string|min:1',
+            'location'=>'required|string|min:1',
+            'dates'=>'required|numeric|digits_between:4,4',
+            'buildingType'=>'required|string|min:1',
+            'description'=>'required|string|min:1',
+            'coordinate1'=>'required|numeric',//Latitude
+            'coordinate2'=>'required|numeric',//Longitude
+
+            'vertices'=>'required|array|min:3',
+            'vertices.*.coordinate1'=>'required|numeric',
+            'vertices.*.coordinate2'=>'required|numeric',
+            'vertices.*.order'=>'required|numeric|min:0',
+
+            'images'=>'nullable|array|min:1',
+            'images.*.image'=>'required|image|max:1000',
+            'images.*.sourceAuthor'=>'required|string|min:1',
+            'images.*.description'=>'required|string|min:1',
+
+            'authors'=>'nullable|array|min:1',
+            'authors.*.name'=>'required|string|min:1'
+
+        ];
+
+        $this->validate($request,$rules);
+
+        $building=Building::create($request->only('buildingName','location','dates','buildingType','description','coordinate1','coordinate2'));
+
+        $vertices=[];
+
+        foreach($request->vertices as $v){
+            array_push($vertices, new Vertice($v));
+        }
+
+        $building->vertices()->delete();
+        $building->vertices()->saveMany($vertices);
+
+
+
+        if($request->images != null){
+            $images=[];
+
+            foreach($request->images as $img){
+
+
+                array_push($images, new Image([
+                    'description'=>$img['description'],
+                    'sourceAuthor'=>$img['sourceAuthor'],
+                    'base64'=>base64_encode(file_get_contents($img['image'])),
+                ]));
+
+            }
+
+            $building->images()->delete();
+            $building->images()->saveMany($images);
+        }
+
+
+        return response()->json(['building'=>$building],200);
 
     }
 
