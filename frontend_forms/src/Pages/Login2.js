d8a176fc242b35baf256a6cbcd72cc35a9cf29fc
tugamars <mars.silva.13@gmail.com>
2020-03-24 23:34:15 +0000

    Updated Login2 to use API script + error handling

--- frontend_forms/src/Pages/Login2.js
+++ frontend_forms/src/Pages/Login2.js
@@ -1,37 +1,49 @@
 import React, { Component } from 'react';
+import ErrorAlert from '../views/Global/ErrorAlert';
 import { Link, NavLink } from 'react-router-dom';
+import usersApi from '../scripts/api/users';
 
 class Login2 extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={email:'', password:'',errors:[]};
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App__Aside"></div>
         <div className="App__Form">
           <div className="PageSwitcher">
-            <NavLink to="/login2" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-            <NavLink exact to="/register2" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            <NavLink to="/login2" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Login</NavLink>
           </div>
 
           <div className="FormTitle">
-            <NavLink to="/login2" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or
-                <NavLink exact to="/register2" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+            <NavLink to="/login2" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Login</NavLink>
           </div>
           <div className="FormCenter">
             <form className="FormFields" onSubmit={this.handleSubmit}>
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" />
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleEmailChange} />
               </div>
 
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="password">Password</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" />
+                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handlePasswordChange}  />
               </div>
 
               <div className="FormField">
-                <button className="FormField__Button mr-20">Sign In</button> <Link to="/register2" className="FormField__Link">Create an account</Link>
+                <button className="FormField__Button mr-20">Login</button> <Link to="/register2" className="FormField__Link">Create an account</Link>
               </div>
             </form>
+
+            <ErrorAlert errors={this.state.errors}/>
+
           </div>          </div>
       </div>
     );
@@ -45,24 +57,23 @@ class Login2 extends Component {
    * password: ......
    * }
    */
-  async login(auth) {
-    let resposta;
+  handleSubmit(e){
+      e.preventDefault();
+      usersApi.login(this.state.email,this.state.password).then( (response) => {
+        console.log(response);
+      }).catch( (error) => {
+        this.setState({errors:error});
+      });
+  }
 
-    resposta = await fetch(
-        "http://psi2020.tugamars.com/api/login",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(auth)
-        }
-    );
+  //
+  handleEmailChange(e){
+    this.setState({email:e.target.value});
+  }
 
-    if (resposta.ok)
-        return await resposta.json();
-    else
-        throw resposta;
+  //
+  handlePasswordChange(e){
+      this.setState({password:e.target.value});
   }
 }
 
