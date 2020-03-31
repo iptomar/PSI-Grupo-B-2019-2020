57a4335fab7a4fe66a5690d11e2cf63ed7a44550
tugamars <mars.silva.13@gmail.com>
2020-03-31 23:50:06 +0100

    Created base page for testing purposes
    Co-Authored-By: danielascserra <danielascserra@users.noreply.github.com>
    Co-Authored-By: bernardoalegria <bernardoalegria@users.noreply.github.com>

---
+++ frontend_forms/src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React, {PropTypes, Component} from 'react';
+
+export default class Home extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            "<div>Home</div>"
+        );
+    }
+}
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
