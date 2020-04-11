33c37965feddc76c85a11d627dddec4fbaf41eea
BernardoAlegria <bernas_alegria@hotmail.com>
2020-04-11 22:23:51 +0100

    Create ponto de interesse. Falta testar com a imagem para ver se funciona

--- frontend_forms/src/scripts/api/pontosDeInteresse.js
+++ frontend_forms/src/scripts/api/pontosDeInteresse.js
@@ -10,11 +10,54 @@ let pontosDeInteresseApi = {
   list() {
 
   },
-
-  create() {
-
+  //dates é em array?
+  /**
+   * 
+   * @param {*} buildName 
+   * @param {*} location  
+   * @param {*} dates 
+   * @param {*} type 
+   * @param {*} description 
+   * @param {*} cc1 
+   * @param {*} cc2 
+   * @param {*} vertices Array de objetos vertices {order:int,coordinate1:double,coordinate2:double}
+   * @param {*} imagens Array de objetos imagens (image,sourceAuthor:string,descrition:string)
+   * @param {*} authors Array de objetos autores {nome:string}
+   * @param {*} routes Array de rotas
+   */
+  create(buildName, location, dates, type, description, cc1, cc2, vertices, imagens, authors, routes) {
+    let furl = apiUrl + "/buildings";
+    let token="Bearer " + localStorage.getItem("auth.token");
+    let body = {
+      "buildingName": buildName,
+      "location": location,
+      "dates": dates,
+      "buildingType": type,
+      "description": description,
+      "coordinate1": cc1,
+      "coordinate2": cc2,
+      "vertices": vertices,
+      "images": imagens,
+      "authors": authors,
+      "routes": routes,
+    };
+    
+    return fetch( furl, {method:'POST',
+                         headers:{'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': token},
+                         body: JSON.stringify(body)
+      }).then((response) => {
+        if (response.ok) {
+          return Promise.resolve(response.json());
+        } else {
+          return Promise.reject(response.json());
+        }
+      });
   },
 
+  /**
+   * Devolve uma promise com os dados de um ponto de interesse
+   * @param {*} id id do edificio
+   */
   get(id) {
     let furl = apiUrl + "/buildings/" + id;
     let token = "Bearer " + localStorage.getItem("auth.token");
@@ -44,12 +87,18 @@ let pontosDeInteresseApi = {
 
 };
 
-
-
-
-
-
-
-
-
 export default pontosDeInteresseApi;
+
+/**********************************************************************************************/
+// Snippets que usei para testar as funções que podem voltar a ser uteis -Bernardo
+/*       
+  *********************** get(id) ***********************************
+  pontosDeInteresseApi.get(3).then( (response) =>{
+            console.log(""+JSON.stringify(response))
+        }).catch( (error) => {
+            console.log("deu problemas")
+        }); 
+
+  ********************** create() ***********************************     
+        
+*/
