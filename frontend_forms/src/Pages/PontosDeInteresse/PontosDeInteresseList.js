80cc2469994112ec8a0abef2d8bf0d4e4a06acde
BernardoAlegria <bernas_alegria@hotmail.com>
2020-04-19 18:53:18 +0100

    Paginação completa na lista de pontos de interesse. Estilo pode ser melhorado no futuro

--- frontend_forms/src/Pages/PontosDeInteresse/PontosDeInteresseList.js
+++ frontend_forms/src/Pages/PontosDeInteresse/PontosDeInteresseList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import pontosDeInteresseApi from '../../scripts/api/pontosDeInteresse';
 import usersApi from '../../scripts/api/users';
+import './CriarPontosInteresse.css';
 
 export default class pontosDeInteresseList extends Component {
 
@@ -8,20 +9,20 @@ export default class pontosDeInteresseList extends Component {
     super(props);
 
     this.state = {
-      pontosDeInteresse: {}
+      "pontosDeInteresse": {},
+      "current_page":1,
+      "last_page":null
     };
 
     usersApi.validateAuth(this.props);
 
-    this.getPontosDeInteresseList(1);
-   
+    this.getPontosDeInteresseList(1);   
   }
 
   render() {
 
     let items = [];
     const pontosDeInteresse = this.state.pontosDeInteresse;
-
     for (let ponto in pontosDeInteresse) {
 
       let i = <tr style={{
@@ -38,8 +39,29 @@ export default class pontosDeInteresseList extends Component {
       </tr>;
 
       items.push(i);
-    }
-    console.log(items);
+    };
+
+    //Paginator TODO: mudar a cor dos botoes?
+    const pagination=[];
+    if(this.state.last_page!==1){
+      //primeiro simbolo (<) existe?
+      if(typeof this.state.current_page !== "undefined" && this.state.current_page !== 1){
+          pagination.push(<li class="page-item"> <a class="page-link" href="#" onClick={()=>this.getPontosDeInteresseList(this.state.current_page-1)}> &lt;</a> </li>);
+      };
+      //numeros para as paginas. TODO: limitar o numero de quadrados possiveis
+      console.log("lastpage",this.state.last_page);
+      for(let i=1;i<=this.state.last_page;i++){
+          pagination.push(<li class="page-item">
+                           <a class="page-link" href="#" onClick={()=>this.getPontosDeInteresseList(i)} >{i}</a>
+                            </li>);
+      };  
+      //segundo simbolo (>) existe?
+      if(this.state.current_page !== this.state.last_page){
+        pagination.push(<li class="page-item">
+                         <a class="page-link" href="#" onClick={()=>this.getPontosDeInteresseList(this.state.current_page+1)}>&gt;</a>
+                       </li>);
+      };
+    };
 
     return (
       <div className="stuff">
@@ -62,13 +84,25 @@ export default class pontosDeInteresseList extends Component {
           </tbody>
         </table>
 
+        <nav aria-label="Page navigation example">
+           <ul class="pagination">
+            {pagination}
+          </ul>
+        </nav>
+
       </div>
     );
   }
 
   getPontosDeInteresseList(page) {
+    if(page < 1){ page=1; } //a pagina minima é 1
+    //se soubermos qual a ultima pagina, e se a pagina atual é maior que a ultima -> a pagina tem de ser a ultima
+    if(this.last_page !== null && page > this.last_page){ page=this.last_page; } 
+
     pontosDeInteresseApi.list(page).then((response) => {
-      this.setState({ pontosDeInteresse: response.data });
+      this.setState({ pontosDeInteresse: response.data,
+                      current_page:response.current_page,
+                      last_page:response.last_page });
     });
   }
 
