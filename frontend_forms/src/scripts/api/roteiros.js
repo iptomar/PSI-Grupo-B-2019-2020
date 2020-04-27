af3a4299e9163c015c947b8f9b49305fd899750e
Daniela Serra <aluno20447@ipt.pt>
2020-04-27 22:40:16 +0100

    Get routes

--- frontend_forms/src/scripts/api/roteiros.js
+++ frontend_forms/src/scripts/api/roteiros.js
@@ -1,4 +1,4 @@
-let apiUrl = process.env.REACT_APP_API_URL_BASE;
+let apiUrl=process.env.REACT_APP_API_URL_BASE;
 
 let roteirosApi = {
 
@@ -17,7 +17,34 @@ let roteirosApi = {
         return Promise.reject(response.json());
       }
     });
-  }
+  },
+
+  get(id) {
+    let furl = apiUrl + "/routes/" + id;
+    let token = "Bearer " + localStorage.getItem("auth.token");
+
+    return fetch(furl, {
+      method: 'GET', headers: {
+        'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': token
+      }
+    }).then((response) => {
+      if (response.ok) {
+        return Promise.resolve(response.json());
+      } else {
+        return Promise.reject(response.json());
+      }
+    })
+  } 
 }
 
+
+roteirosApi.get(5).then( (response) =>{
+  console.log("" + JSON.stringify(response))
+}).catch( (error) => {
+  console.log("deu problemas")
+});
+
 export default roteirosApi;
+
+
+
