89e5ae7fdae0dbd5d94d7f6344ff238e24d96355
Tiago Vasconcelos <49621982+tiagorafael982@users.noreply.github.com>
2020-05-02 15:25:17 +0100

    Adicionar pontos de interesse na página Criar Roteiros.

--- frontend_forms/src/Pages/Routes/CreateRoutes.js
+++ frontend_forms/src/Pages/Routes/CreateRoutes.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
 import ErrorAlert from '../../views/Global/ErrorAlert';
 import roteirosApi from '../../scripts/api/roteiros';
-
+import pontosDeInteresseApi from '../../scripts/api/pontosDeInteresse';
 
 class CreateRoutes extends Component {
     constructor(props){
         super(props);
         this.state = {
-            nameRoute: '',
+            nameRoute: '', pointsOfInteresPage: 1, pointsOfInteresPageMax: 99, pointsOfInteresList: {}, selectedPointsOfInterest:null, pointsOfInterest: [],
             errors: []
-        }
+        } 
 
         this.handleNameRoute = this.handleNameRoute.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.addPointsOfInterest = this.addPointsOfInterest.bind(this);
+        this.handlePointsOfInterestChange = this.handlePointsOfInterestChange.bind(this);
+
+        this.getPointsOfInterest(1);
     }
 
     render(){
+
+        let listaPointOfInterest = [];
+        const pontosDeInteresse = this.state.pointsOfInterest;
+        for( let ponto in pontosDeInteresse) {
+            let i = <tr style = {{ textAlign: "center"}}
+                        key = {"ponto" + ponto}>
+                            <td>{this.state.pointsOfInteresList[pontosDeInteresse[ponto]].buildingName}</td>
+                    <td>
+                        <button type="button" class="btn btn-danger" onClick={() => this.deletePointOfInterest(ponto)}>Delete</button>
+                    </td>
+            </tr>
+        listaPointOfInterest.push(i);
+        console.log(pontosDeInteresse);
+        };
+
+
+        const options = [];
+        const pontosDeInteresseExistentes = this.state.pointsOfInteresList;
+        let p;
+        for( let ponto in pontosDeInteresseExistentes) {
+            p = <option value={pontosDeInteresseExistentes[ponto].id}>{pontosDeInteresseExistentes[ponto].buildingName}</option>
+            options.push(p);
+        };
+
         return(
         <div className="fundo" >
 				<form className="needs-validation" onSubmit={this.handleSubmit}>
@@ -24,15 +52,37 @@ class CreateRoutes extends Component {
 						<label for="nameRoute"><b>Name</b></label>
 						<input className="form-control" type="text" placeholder="Insert name..." name="nameRoute" value={this.state.nameRoute} onChange={this.handleNameRoute} required />
 					</div>
-
+					<div className="form-group row">
+						<label for="pontos"><b>Points Of Interest</b></label>
+					</div>
+                    <div className="tabelaPontos">
+						<table className="table table-hover table-dark table-striped rounded" id="pontosDeInteresseExistentes">
+							<thead>
+								<tr style={{
+									textAlign: "center"
+								}}>
+									<th scope="col" >Ponto de Interesse</th>
+								</tr>
+							</thead>
+							<tbody>
+								{listaPointOfInterest}
+							</tbody>
+						</table>
+                    </div>
+                    <div>
+						<button type="submit" value="submit" onClick={this.addPointsOfInterest}>Add point</button>
+						<select onChange={this.handlePointsOfInterestChange}>
+							{options}
+						</select>
+					</div>
 					<div className="form-group col"></div>
 					<hr class="mb-3"></hr>
 					<button className="btn-lg btn-dark btn-block" type="submit" value="submit" onClick={this.handleSubmit} name="create">
 						Submit
-                </button>
+                    </button>
 					<ErrorAlert errors={this.state.errors} />
 				</form>
-			</div>
+		</div>
         );
     }
 
@@ -48,6 +98,44 @@ class CreateRoutes extends Component {
     handleNameRoute(e){
         e.preventDefault();
         this.setState({nameRoute:e.target.value});
+    }
+
+    handlePointsOfInterestChange(e) {
+        this.setState({selectedPointsOfInterest: e.target.value});
+    }
+
+    addPointsOfInterest(e) {
+        e.preventDefault();
+        if(this.state.selectedPointsOfInterest !== null) {
+            this.setState({
+                pointsOfInterest: [...this.state.pointsOfInterest, this.state.selectedPointsOfInterest]
+            });
+        }
+    }
+
+    getPointsOfInterest(page) {
+        pontosDeInteresseApi.list(page).then((response) => {
+            let arrayFinal = {};
+
+            for(let k in response.data) {
+                console.log("teste");
+                if(response.data.hasOwnProperty(k)) {
+                    arrayFinal[response.data[k].id] = response.data[k];
+                }
+            }
+
+            this.setState({
+                pointsOfInteresList: arrayFinal, 
+                                    pointsOfInteresPage: response.current_page, 
+                                    pointsOfInteresPageMax: response.last_page
+            });
+        });
+    }
+
+    deletePointOfInterest(index) {
+        let aux1 = this.state.pointsOfInterest;
+        aux1.splice(index,1);
+        this.setState({pointsOfInterest:aux1});
     }
 
 }
