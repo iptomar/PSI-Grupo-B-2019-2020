0ff2bc30381f554bcf3c24bda09e3bb63dcb53ae
Daniela Serra <aluno20447@ipt.pt>
2020-05-05 23:28:49 +0100

    Delete authors/id

--- frontend_forms/src/scripts/api/authors.js
+++ frontend_forms/src/scripts/api/authors.js
@@ -88,17 +88,41 @@ let authorsApi = {
                     return Promise.reject(response.json());
                   }
                 });
+              },
+
+              delete(id){
+                let furl = apiUrl + "/authors/" + id;
+                let token = "Bearer " + localStorage.getItem("auth.token");
+                return fetch(furl, {
+                  method: 'DELETE', 
+                  headers: {
+                    'Content-Type': 'application/json', 
+                    'Accept': 'application/json', 
+                    'Authorization': token
+                  }
+                }).then((response) => {
+                  if (response.ok) {
+                    return Promise.resolve(response.json());
+                  } else {
+                    return Promise.reject(response.json());
+                  }
+                });
               }
     
     };
 
-authorsApi.update(5).then( (response) =>{
+authorsApi.delete(11).then( (response) =>{
+  console.log("" +JSON.stringify(response))
+}).catch( (error) => {
+  console.log("deu problemas")
+});
+
+
+/*authorsApi.update(5).then( (response) =>{
   console.log(""+JSON.stringify(response))
   }).catch( (error) => {
   console.log("deu problemas: "+JSON.stringify(error))
-  });
-
-
+  });*/
 
  /*   authorsApi.get(5).then( (response) =>{
   console.log("" + JSON.stringify(response))
