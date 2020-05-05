0ec4203c179fde5898188203652254372c0dec0f
Daniela Serra <aluno20447@ipt.pt>
2020-05-05 23:21:59 +0100

    Get authors/id

--- frontend_forms/src/scripts/api/authors.js
+++ frontend_forms/src/scripts/api/authors.js
@@ -47,15 +47,40 @@ let authorsApi = {
                     });
                 });
         
-            }
-    
-};
+            },
 
-let name = "Salazar";
+            get(id) {
+                let furl = apiUrl + "/authors/" + id;
+                let token = "Bearer " + localStorage.getItem("auth.token");
+                return fetch(furl, {
+                  method: 'GET', headers: {
+                    'Content-Type': 'application/json', 
+                    'Accept': 'application/json', 
+                    'Authorization': token
+                  }
+                }).then((response) => {
+                  if (response.ok) {
+                    return Promise.resolve(response.json());
+                  } else {
+                    return Promise.reject(response.json());
+                  }
+                })
+              }
+    
+    };
+
+
+    authorsApi.get(5).then( (response) =>{
+  console.log("" + JSON.stringify(response))
+}).catch( (error) => {
+  console.log("deu problemas:" +JSON.stringify(error))
+});
+
+/*let name = "Salazar";
 authorsApi.create(name).then( (response) =>{
 console.log(""+JSON.stringify(response))
 }).catch( (error) => {
 console.log("deu problemas: "+JSON.stringify(error))
-});
+});*/
 
 export default authorsApi;
